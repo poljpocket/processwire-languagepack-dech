aa90c8b724d1b9e56049e235b8d9823cf9ac60d0
Julian Pollak <j.pollak@fruitcake.ch>
2023-09-26 16:43:49 +0200

    convert to SwissGerman.info.php

---
+++ SwissGerman.info.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace ProcessWire;
+
+$info = array(
+    'title' => 'Swiss German',
+    'summary' => 'Swiss German language pack for ProcessWire using non-formal language',
+    'version' => 12,
+    'author' => 'poljpocket',
+    'href' => 'https://github.com/poljpocket/processwire-languagepack-dech',
+);
